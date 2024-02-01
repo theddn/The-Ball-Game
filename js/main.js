72b530d67fe1d7ef3b72ffe55b358c6a42ad6d43
theddn <theddn@gmail.com>
2024-02-01 10:12:13 +0200

    Limit ball growth

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var ballStep = 50
 
 function onBallClick(elBall) {
     ballSize += ballStep
-    
+    if (ballSize > 400) ballSize = 100
+
     elBall.innerText = ballSize
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
